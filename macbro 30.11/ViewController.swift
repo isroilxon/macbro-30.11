c92e0d0c1465bdb23df36202d51ce2847ed5e1b1
isroil <5085624@mail.ru>
2022-12-01 00:14:20 +0500

    Initial Commit

---
+++ macbro 30.11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  macbro 30.11
+//
+//  Created by mac on 30/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ macbro 30.11/secondview/secondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  secondViewController.swift
+//  macbro 30.11
+//
+//  Created by mac on 30/11/22.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
